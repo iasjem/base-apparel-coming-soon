49b18db47f86bc2fba0b6d5686b817b6c9b583be
iasjem <jemimah.beryl.sai@gmail.com>
2020-04-12 20:43:17 +0800

    Refactor script file for code readability - js - refactor

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ const btnSendEmail = document.getElementById('btn_send_email');
 const spanInvalidEmailMessage = document.getElementById('span_invalid_email');
 const iconInvalidEmail = document.getElementById('icon_invalid_email');
 
-inputEmail.addEventListener('keydown', function () {
-    let isTaggedInvalid = inputEmail.classList.contains('invalid-email');
+const showInvalidMessage = () => {
+    inputEmail.classList.add('invalid-email');
+    spanInvalidEmailMessage.style.display = "block";
+    iconInvalidEmail.style.display = "block";
+};
 
-    if (isTaggedInvalid == true) {
-        inputEmail.classList.remove('invalid-email');
-        spanInvalidEmailMessage.style.display = "none";
-        iconInvalidEmail.style.display = "none";
-    } else {
-        // do nothing
-    }
-});
+const clearInvalidMessage = () => {
+    inputEmail.classList.remove('invalid-email');
+    spanInvalidEmailMessage.style.display = "none";
+    iconInvalidEmail.style.display = "none";
+};
 
-btnSendEmail.addEventListener('click', function () {
+const validateEmail = () => {
     let email = inputEmail.value;
     let isTaggedInvalid = inputEmail.classList.contains('invalid-email');
     let isValidEmail = /([A-z]{1,}|[A-z]{2,}_[A-z0-9]{1,})@([a-z]{3,15})\.([a-z]{2,5})$/.test(email);
@@ -24,16 +24,25 @@ btnSendEmail.addEventListener('click', function () {
     if (isValidEmail == true) {
         modalEmailSuccess.modal('show');
     } else if (isTaggedInvalid == false && isValidEmail == false) {
-        inputEmail.classList.add('invalid-email');
-        spanInvalidEmailMessage.style.display = "block";
-        iconInvalidEmail.style.display = "block";
+        showInvalidMessage();
     } else if (isTaggedInvalid == true && isValidEmail == true) {
-        inputEmail.classList.remove('invalid-email');
-        spanInvalidEmailMessage.style.display = "none";
-        iconInvalidEmail.style.display = "none";
-
+        clearInvalidMessage();
         modalEmailSuccess.modal('show');
     } else {
         // do nothing
     }
+};
+
+inputEmail.addEventListener('keydown', function () {
+    let isTaggedInvalid = inputEmail.classList.contains('invalid-email');
+
+    if (isTaggedInvalid == true) {
+        clearInvalidMessage();
+    } else {
+        // do nothing
+    }
+});
+
+btnSendEmail.addEventListener('click', function () {
+    validateEmail();
 });
